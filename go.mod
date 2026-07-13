5fd184b6d92aa9c4722ff97b361e8fe5ebb8967e
qbao <dangquocbao9504@gmail.com>
2026-07-13 19:00:00 +0700

    realize not need urfave cli and remove

--- go.mod
+++ go.mod
@@ -1,5 +1,3 @@
 module commit2commit
 
 go 1.25.1
-
-require github.com/urfave/cli/v3 v3.10.1 // indirect
--- main.go
+++ main.go
@@ -1,24 +1,4 @@
 package main
 
-import (
-	"context"
-	"fmt"
-	"log"
-	"os"
-
-	"github.com/urfave/cli/v3"
-)
-
 func main() {
-	cmd := &cli.Command{
-		Name:  "c2c",
-		Usage: "read code easier",
-		Action: func(ctx context.Context, c *cli.Command) error {
-			fmt.Println("source code")
-			return nil
-		},
-	}
-	if err := cmd.Run(context.Background(), os.Args); err != nil {
-		log.Fatal(err)
-	}
 }
